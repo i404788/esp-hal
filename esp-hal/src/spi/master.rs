b423321986292ca4557e32fed1e7240a6d5d9bd7
ferris <ferris@devdroplets.com>
2024-12-23 12:46:23 +0100

    Less debug

--- esp-hal/src/spi/master.rs
+++ esp-hal/src/spi/master.rs
@@ -1091,7 +1091,7 @@ mod dma {
         fn is_done(&self) -> bool {
             if self.tx_transfer_in_progress && !self.channel.tx.is_done() {
                 assert!(!self.channel.tx.has_error(), "Got DMA descriptor error while waiting for completion");
-                debug!("tx busy, intrs:");
+                // debug!("tx busy, intrs:");
                 for pend in self.channel.tx.pending_out_interrupts().iter() {
                     debug!("Intr: {}", pend);
                 }
